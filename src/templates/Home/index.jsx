ae86f54f6175bcc3c6a7f96bb42581facf82530a
Cauan Boss <cauanboss@gmail.com>
2022-01-08 22:28:35 -0300

    Meu hook async

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,10 +1,49 @@
+import { useCallback, useEffect, useState } from 'react';
+
+const useAsync = (asyncFunction, shouldRun) => {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('idle');
+
+  const run = useCallback(() => {
+    setResult(null);
+    setError(null);
+    setStatus('pending');
+
+    return asyncFunction()
+      .then((response) => {
+        setStatus('settled');
+        setResult(response);
+      })
+      .catch((error) => {
+        setStatus('error');
+        setError(error);
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (shouldRun) {
+      run();
+    }
+  }, [run, shouldRun]);
+
+  return [run, result, error, status];
+};
+
+const fetchData = async () => {
+  let data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let json = await data.json();
+  return json;
+};
 
 const Home = () => {
-  return (
-    <div className="App">
-      <h1></h1>
-    </div>
-  );
-}
+  const [reFetchData, result, error, status] = useAsync(fetchData, true);
+
+  // useEffect(() => {
+  //   reFetchData();
+  // }, [reFetchData]);
+
+  return <p>{JSON.stringify(result)}</p>;
+};
 
 export default Home;
